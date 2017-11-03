9b2ff726d2d89fad280451fbb2a5db0910e8bac1
zenwarr <zenw@yandex.ru>
2017-11-03 21:42:45 +0600

    add slider dragging

--- src/client/components/slider/slider.tsx
+++ src/client/components/slider/slider.tsx
@@ -11,17 +11,32 @@ export interface SliderProps {
   transition?: boolean;
 }
 
-export class Slider extends React.Component<SliderProps> {
+export interface SliderState {
+  dragStartPos: number;
+  dragging: boolean;
+  overridePercent: number;
+}
+
+export class Slider extends React.Component<SliderProps, SliderState> {
+  constructor(props: SliderProps) {
+    super(props);
+    this.state = {
+      dragStartPos: -1,
+      dragging: false,
+      overridePercent: -1
+    };
+  }
+
   render() {
     let classes = cn('slider', this.props.extraClasses,
         this.props.enabled === false ? 'slider--disabled' : null,
-        this.props.transition ? 'slider--transition' : null);
+        this.props.transition && !this.state.dragging ? 'slider--transition' : null);
+    let percent = this.state.overridePercent >= 0 ? this.state.overridePercent : this.props.percent;
+
     return <label className={classes} onClick={this.onClick}>
-      <input type="range" className={"slider__input"} min={0} max={100} value={this.props.percent}
-             disabled={this.props.enabled === false} />
-      <div className={"slider__track"}>
-        <div className={"slider__elapsed"} style={{width: this.props.percent + '%'}}>
-          <span className={"slider__handle"} />
+      <div className={"slider__track"} ref={track => this.track = track}>
+        <div className={"slider__elapsed"} style={{width: percent + '%'}}>
+          <span className={"slider__handle"} onMouseDown={this.onMouseDown} onDragStart={this.onDragStart} />
         </div>
       </div>
     </label>;
@@ -29,11 +44,107 @@ export class Slider extends React.Component<SliderProps> {
 
   @autobind
   onClick(e: React.MouseEvent<HTMLElement>) {
-    if (this.props.enabled !== false && this.props.onChange) {
-      let elem_pos = e.pageX - (e.currentTarget.getBoundingClientRect().left + document.body.scrollLeft);
-      let elem_width = e.currentTarget.getBoundingClientRect().width;
-      let percent_pos = (elem_pos / elem_width) * 100;
-      this.props.onChange(percent_pos);
+    if (this.props.enabled) {
+      this.setPercent(Slider.percentFromClientX(e.clientX, e.currentTarget));
     }
   }
+
+  @autobind
+  onDragStart(e: React.MouseEvent<HTMLElement>) {
+    e.preventDefault();
+  }
+
+  @autobind
+  onMouseDown(e: React.MouseEvent<HTMLElement>) {
+    if (this.props.enabled !== false && this.track) {
+      this.startHandleDrag(e.clientX, this.track);
+    }
+  }
+
+  @autobind
+  onMouseUp(e: React.MouseEvent<HTMLElement>) {
+    if (this.state.dragging === true) {
+      this.endHandleDrag();
+    }
+  }
+
+  @autobind
+  onBlur(e: React.FocusEvent<HTMLElement>) {
+    if (this.state.dragging === true) {
+      this.endHandleDrag();
+    }
+  }
+
+  /**
+   * This event is response to events fired on document element.
+   * @param {React.MouseEvent<HTMLElement>} e
+   */
+  @autobind
+  onMouseMove(e: React.MouseEvent<HTMLElement>) {
+    if (this.props.enabled !== false && this.state.dragging && this.track) {
+      this.setState({
+        overridePercent: Slider.percentFromClientX(e.clientX, this.track)
+      });
+    }
+  }
+
+  protected startHandleDrag(clientX: number, track: HTMLElement): void {
+    let percent = Slider.percentFromClientX(clientX, track);
+    this.setState({
+      dragging: true,
+      dragStartPos: clientX,
+      overridePercent: percent
+    });
+
+    const mouseMoveHandler = this.onMouseMove.bind(this);
+    const blurHandler = () => {
+      this.onBlur.apply(this, arguments);
+    };
+    const mouseUpHandler = () => {
+      window.removeEventListener('mousemove', mouseMoveHandler);
+      window.removeEventListener('mouseup', mouseUpHandler);
+      window.removeEventListener('blur', blurHandler);
+      this.onMouseUp.apply(this, arguments);
+    };
+
+    window.addEventListener('mousemove', mouseMoveHandler, true);
+    window.addEventListener('mouseup', mouseUpHandler, true);
+    window.addEventListener('blur', blurHandler);
+  }
+
+  protected endHandleDrag(): void {
+    this.setState({
+      dragging: false,
+      dragStartPos: -1
+    });
+    this.setPercent(this.state.overridePercent);
+  }
+
+  /**
+   * Calculates percent value from mouse position in client coordinates.
+   * @param {number} clientX Client coordinates of mouse position
+   * @param {HTMLElement} track Track element
+   * @returns {number} Calculated percent value
+   */
+  protected static percentFromClientX(clientX: number, track: HTMLElement): number {
+    let trackRect = track.getBoundingClientRect();
+    if (clientX <= trackRect.left) {
+      return 0;
+    } else if (clientX >= trackRect.right) {
+      return 100;
+    } else {
+      return ((clientX - trackRect.left) / trackRect.width) * 100;
+    }
+  }
+
+  protected setPercent(value: number): void {
+    this.setState({
+      overridePercent: value
+    });
+    if (this.props.enabled !== false && this.props.onChange) {
+      this.props.onChange(value);
+    }
+  }
+
+  protected track: HTMLElement|null = null;
 }
--- src/client/components/utils.ts
+++ src/client/components/utils.ts
@@ -1,3 +1,24 @@
 export function cn(base: string, ...extra: (string|null|undefined)[]): string {
   return base + ' ' + extra.map(x => x ? x : '').join(' ');
 }
+
+export function clientXToDoc(viewportX: number): number {
+  return viewportX + (document.scrollingElement || document.documentElement).scrollLeft;
+}
+
+export function clientYToDoc(viewportY: number): number {
+  return viewportY + (document.scrollingElement || document.documentElement).scrollTop;
+}
+
+export interface Position {
+  left: number;
+  top: number;
+}
+
+export function elemDocPosition(elem: HTMLElement): Position {
+  let boundingRect = elem.getBoundingClientRect();
+  return {
+    left: clientXToDoc(boundingRect.left),
+    top: clientYToDoc(boundingRect.top)
+  };
+}
